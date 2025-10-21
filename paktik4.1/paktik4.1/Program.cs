589ebd99a52057de5bb4cab5924b599ec05cd020
icha258753-cloud <icha258753@student.smkn1kandeman.sch.id>
2025-10-21 09:55:36 +0700

    Add files via upload

---
+++ paktik4.1/paktik4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paktik4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Memeriksa apakah seseorang sudah remaja
+            Console.WriteLine("Masukkan usia anda   : ");
+            int usia = int.Parse(Console.ReadLine());
+            if (usia >= 13);
+            Console.WriteLine("Anda sudah memasuki usia remaja.");
+        }
+    }
+}
