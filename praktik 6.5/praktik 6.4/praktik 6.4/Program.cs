f0f83ea95bedadb90e12eea06dbdce3d8c8cdf93
icha258753-cloud <icha258753@student.smkn1kandeman.sch.id>
2025-10-21 10:09:46 +0700

    Add files via upload

---
+++ praktik 6.5/praktik 6.4/praktik 6.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_6._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Inisialisasi variabel penghitung(counter)
+            int hitungan = 5;
+
+            Console.WriteLine("Memulai hitungan mundur:");
+            //2. Perulangan 'while'
+            // Kondisi: Selama nilai 'hitungan' lebih brsaer dari 0
+            while (hitungan > 0)
+            {
+                // Blok kode di dalam perulangan
+                Comsole.WriterLine($"Hitungan: {hitungan}");
+
+                //3. Update variabel penghitung (decrement/pengurangan)
+                // Ini PENTING agar perulangan tidak menjadi loop tak terbatas (infinite loop)
+                hitungan = hitungan - 1;
+                // atau bisa juga ditulis: hitungaan--;
+            }
+            // Kode setelah perulangan selesai 
+            Console.WriteLine("Selesai! Hitungan mundur telah berakhir.");
+    }
+}
