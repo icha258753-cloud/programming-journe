699f52f15a459d177295c58a3ace139a3251365a
icha258753-cloud <icha258753@student.smkn1kandeman.sch.id>
2025-10-21 15:19:09 +0700

    Add files via upload

---
+++ PRAKTIK4.1_KINAURA/PRAKTIK4.1_KINAURA/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRAKTIK4._1_KINAURA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ptaktik 7.a/ptaktik 7.a/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ptaktik_7.a
+{
+    internal class Program
+    {
+        static void sapa()
+        {
+            Console.WriteLine("Hallo, selamat datang dipembelajaran C#!");
+            Console.WriteLine("Mari belajar tentang fungsi!");
+        }
+        static void Main(string[] args)
+        {
+            sapa(); // memanggil fungsi
+        }
+    }
+}
+  
