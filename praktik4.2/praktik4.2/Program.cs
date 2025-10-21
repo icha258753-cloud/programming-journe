d537b96a2c13624a57ca9cc70df934f5001ff8a5
icha258753-cloud <icha258753@student.smkn1kandeman.sch.id>
2025-10-21 09:57:42 +0700

    Add files via upload

---
+++ praktik4.2/praktik4.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Memeriksa apakah sebuah bilangan positif
+            Console.WriteLine("Masukan suhunya  : ");
+            double suhu = double.Parse(Console.ReadLine());
+            if (suhu > 0)
+            Console.WriteLine("Suhu diatas titik beku (positif).");
+        }
+    }
+}
---
+++ praktik4.2/praktik4.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("praktik4.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("praktik4.2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("17a65424-717f-436c-ad70-409b7c6eb1ac")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
