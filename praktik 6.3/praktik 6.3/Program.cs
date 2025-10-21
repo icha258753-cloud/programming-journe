e65a680039f4593182dedd122054958aae35d2c8
icha258753-cloud <icha258753@student.smkn1kandeman.sch.id>
2025-10-21 10:08:29 +0700

    Add files via upload

---
+++ praktik 6.3/praktik 6.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_6._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inisialisasi varibel untuk menyimpan input
+            string inputUser = "";
+
+            Console.WriteLine("Program akan terus berjalan hingga Anda mengetik 'keluar' .");
+
+            // Perulangan selama isi variabel inputUser  TIDAK SAMA DDENGAN "keluar"
+            while (inputUser.ToLower() != "keluar")
+            {
+                Console.WriteLine("\nKetik sesuatu (atau 'keluar' untuk berhenti): ");
+                inputUser = Console.ReadLine(); // Baca  input dari user
+
+                Console.WriteLine("Anda memgetik: " + inputUser);
+            }
+            Console.WriteLine("\nProgram selesai. Terima kasih!");
+        }
+    }
+}
