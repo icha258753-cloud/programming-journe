f094324434d471c9ef75f88a6ee54ff04cf2315b
icha258753-cloud <icha258753@student.smkn1kandeman.sch.id>
2025-11-26 07:53:06 +0700

    Add files via upload

---
+++ icha alsyaffa sessa x pplg2/icha alsyaffa sessa x pplg2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+internal class Program
+{
+    // Data kereta menggunakan array
+    static string[] kodeKereta = { "SEN", "MAL", "KUT" };
+    static string[] namaKereta = { "SENJA", "MALAM", "KUTARA" };
+    static int[] hargaKereta = { 150000, 200000, 250000 };
+    static string[] jamKereta = { "20.00", "18.30", "07.15" };
+
+    // Method hitung total
+    static int HitungTotal(int harga, int jumlah)
+    {
+        return harga * jumlah;
+    }
+    static void Main(string[] args)
+    {
+        // Method menampilkan laporan
+        void TampilLaporan(string kode, string jenis, int harga, string jam, int jumlah, int total)
+        {
+            Console.WriteLine("\n===== LAPORAN PENJUALAN TIKET =====");
+            Console.WriteLine($"Kode Tiket           : {kode}");
+            Console.WriteLine($"Jenis Kereta         : {jenis}");
+            Console.WriteLine($"Harga Tiket          : {harga}");
+            Console.WriteLine($"Jam Keberangkatan    : {jam}");
+            Console.WriteLine($"Jumlah Pembelian     : {jumlah}");
+            Console.WriteLine($"Total Harga          : {total}");
+            Console.WriteLine("====================================\n");
+        }
+
+
+        {
+            string ulang = "y";
+
+            while (ulang == "y" || ulang == "Y")
+            {
+                Console.Clear();
+                Console.WriteLine("============================================");
+                Console.WriteLine("      PENJUALAN TIKET KERETA API");
+                Console.WriteLine("  Oleh: Icha Alsyaffa Sessa");
+                Console.WriteLine("============================================\n");
+
+                // Input kode tiket
+                Console.Write("Masukkan Kode Tiket (SEN/MAL/KUT) : ");
+                string kodeInput = Console.ReadLine().ToUpper();
+
+                int index = -1;
+
+                // Cari kereta sesuai kode
+                for (int i = 0; i < kodeKereta.Length; i++)
+                {
+                    if (kodeInput.Contains(kodeKereta[i]))
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+
+                // Jika kode tidak ditemukan
+                if (index == -1)
+                {
+                    Console.WriteLine("\nKode tiket tidak ditemukan!");
+                    Console.Write("Ulangi program? (y/n): ");
+                    ulang = Console.ReadLine();
+                    continue;
+                }
+
+                int harga = hargaKereta[index];
+                string jenis = namaKereta[index];
+                string jam = jamKereta[index];
+
+                // Input jumlah pembelian
+                Console.Write("Masukkan Jumlah Tiket : ");
+                int jumlahTiket = int.Parse(Console.ReadLine());
+
+                // Hitung total
+                int totalBayar = HitungTotal(harga, jumlahTiket);
+
+                // Tampilkan laporan
+                TampilLaporan(kodeInput, jenis, harga, jam, jumlahTiket, totalBayar);
+
+                Console.Write("Proses lagi? (y/n): ");
+                ulang = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nTerima kasih telah menggunakan aplikasi!");
+        }
+    }
+}
---
+++ icha alsyaffa sessa x pplg2/icha alsyaffa sessa x pplg2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("icha alsyaffa sessa x pplg2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("icha alsyaffa sessa x pplg2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ea957de-dc1f-4917-b1d1-2b84c29b2941")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
